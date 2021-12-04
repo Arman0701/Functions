da3af278950178f4a7cc8ab8d171df4cf2722216
Arman773 <tadevosyan889@gmail.com>
2021-12-04 20:46:53 +0400

    added task 4

---
+++ Required_Tasks/4.Check-correct-sublist.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// Given a word and a list of possible anagrams, select the correct sub list.
+
+function checking(word, arr){
+let result =[]
+    for(let i=0;i<arr.length;i++){
+        if(anagrams(word,arr[i])) result.push(arr[i])
+    }
+    function anagrams(stringA, stringB) {
+        return cleanString(stringA) === cleanString(stringB);
+    }
+    
+    function cleanString(str) {
+        return str.replace(/[^\w]/g).toLowerCase().split('').sort().join()
+    } 
+    return result 
+}
+
+console.log(checking("listen", ["enlists", "google", "inlets", "banana"]));
+console.log(checking("pencil", ["licnep", "circular", "pupil", "nilcpe", "leppnec"]));
