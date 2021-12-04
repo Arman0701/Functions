6d077e118669c2164beca807956c5c8d3f80e640
Arman773 <tadevosyan889@gmail.com>
2021-12-05 00:57:12 +0400

    added task 17

---
+++ Required_Tasks/17.Shallow-compare.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// Given two objects. Write a function that performs shallow compare.
+
+function shallowCompare(a, b){
+    let keys1 = Object.keys(a);
+    let keys2 = Object.keys(b);
+
+    if (keys1.length !== keys2.length) return false;
+    else return true;
+}
+
+console.log(shallowCompare({a: '1'}, {a: '1'}));
+console.log(shallowCompare({a: '1'}, {a: '1', b: '2'}));
+console.log(shallowCompare({}, {}));
