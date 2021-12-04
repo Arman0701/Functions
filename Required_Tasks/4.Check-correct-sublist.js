ff577b720c649b799a1e287b817615f3cda22f80
Arman773 <tadevosyan889@gmail.com>
2021-12-04 20:54:40 +0400

    fixed issue in task 4

--- Required_Tasks/4.Check-correct-sublist.js
+++ Required_Tasks/4.Check-correct-sublist.js
@@ -3,16 +3,17 @@
 // Given a word and a list of possible anagrams, select the correct sub list.
 
 function checking(word, arr){
-let result =[]
+    let result =[];
+
     for(let i=0;i<arr.length;i++){
-        if(anagrams(word,arr[i])) result.push(arr[i])
+        if(anagrams(word,arr[i])) result.push(arr[i]);
     }
     function anagrams(stringA, stringB) {
         return cleanString(stringA) === cleanString(stringB);
     }
     
     function cleanString(str) {
-        return str.replace(/[^\w]/g).toLowerCase().split('').sort().join()
+        return str.replace(/[^\w]/g).toLowerCase().split('').sort().join();
     } 
     return result 
 }
